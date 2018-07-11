7c114d63e50af046ba420cc9e20185cb11906ebd
Rahul Vijay <vk.rahul318@gmail.com>
2018-07-11 17:02:04 +0530

    added save and load

--- Self_Driving_Car/ai.py
+++ Self_Driving_Car/ai.py
@@ -112,16 +112,63 @@ class Dqn():
         # Update the weights
         self.optimizer.step()
         
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+    # Update the elements and keep transitions going
+    def update(self, reward, new_signal):
+        # new state composed with the 3 signals 
+        new_state = torch.Tensor(new_signal).float().unsqueeze(0)
+        # update the memory 
+        self.memory.push((self.last_state, new_state, torch.LongTensor([int(self.last_action)])), torch.Tensor([self.last_reward]))
+        # play an action
+        action=  self.select_action(new_state)
+        if len(self.memory.memory) > 100:
+            batch_state, batch_next_state, batch_reward, batch_action = self.memory.sample(100)
+            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
+        self.last_action = action
+        self.last_state = new_state
+        self.last_reward = reward
+        self.reward_window.append(reward)
+        if len(self.reward_window) > 1000:
+            del self.reward_window[0]
+        return action
+    
+    # compute the mean of all rewards in the reward_window
+    def score(self):
+        return sum(self.reward_window)/(len(self.reward_window)+1)
+    
+    # save the model 
+    def save(self):
+        # save the most recently used weights
+        torch.save({'state_dict': self.model.state_dict(),
+                    'optimizer': self.optimizer.state_dict,
+                    }, 'last_brain.pth')
+    
+    # load function
+    def load(self):
+        if os.path.isfile('last_brain.pth'):
+            print("=> loading checkpoint ...")
+            checkpoint = torch.load('last_brain.pth')
+            # update the model
+            self.model.load_state_dict(checkpoint['state_dict'])
+            # update the parameters
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print('done !')
+        else:
+            print("No checkpoint found ...")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+        
+        
